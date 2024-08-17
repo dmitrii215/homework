31267c6f86027350d49531f0a4a059ea5f4cdb36
user <stepanovda@bk.ru>
2024-08-18 00:38:46 +0300

    базовые структуры данных (задача средний балл)

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-name= "Дмитрий"
-print(name)
-age= 50
-age=age+5
-print(age)
-'is_student: True'
-print('is_student: True')
+grades=[[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3,],[5,5,5,4,5]]
+studens={'Johnni','Bilbo','Steve','khendrik','aaron'}
+
+grades_a = [sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]),
+            sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])]
+
+studens_sort = sorted(studens)
+
+dict1 = dict(zip(studens_sort,grades_a))
+print(dict1)
