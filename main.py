b7ea0d94f0b04d800d693bebb96665564b26fb85
Дмитрий <stepanovda.@bk.ru>
2024-09-10 13:46:45 +0300

    жучек

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-'''множества'''
-a= {1, 2, 3, 3, 4, 4, 5, False, 'str'}
+from time import sleep
+
+a = 5
 print(a)
+print("я тут")
+sleep(4)
+print("фуух 4 секунды прошло")
 
-b = {1, 2, 3, True, (1, 2, 3)}
-print(b)
-
-list = [1, 2, 3, 4]
-print(set(list))
