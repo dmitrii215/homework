d791731e341cffa74755db69830f82d52576abf3
Дмитрий <stepanovda.@bk.ru>
2024-09-23 10:53:51 +0300

    задача область видимости

--- module_4.2 пространство имен.py
+++ module_4.2 пространство имен.py
@@ -1,5 +1,9 @@
 def test_function():
     def inner_function():
-        nonlocal()
-        print('я в области видимости test_function')
+        print("Я в области видимости функции test_function")
     inner_function()
+    print(inner_function())
+    pass
+test_function()
+
+# inner_function() Я не могу вызвать её вне функции test_function, т.к они в разных пространствах
