2ada4a2f3f12852a66dd1bd4f911aaa5ec13bdb4
Дмитрий <stepanovda.@bk.ru>
2024-10-22 19:03:35 +0300

    долги

---
+++ module_1.6py.py
@@ -0,0 +1,25 @@
+'''словарь'''
+
+my_dict = {'Дмитрий': 1973, 'Маша': 1975}
+print(my_dict)
+
+print(my_dict['Дмитрий'])
+
+my_dict.update({'Паша': 1988, 'Катя': 2002})
+print(my_dict)
+
+del my_dict['Маша']
+print(my_dict)
+
+'''множества'''
+
+my_set = {1, 2, 3, 2, 3,  'str', 'str', True}
+print(my_set)
+
+my_set.add('4')
+my_set.add(5)
+my_set.add(False)
+print(my_set)
+
+my_set.remove(2)
+print(my_set)
