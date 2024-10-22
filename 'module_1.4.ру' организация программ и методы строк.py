2daf96eb071068e6d98866989ba740d9ca39b314
Дмитрий <stepanovda.@bk.ru>
2024-10-22 14:44:11 +0300

    долги

--- 'module_1.4.ру' организация программ и методы строк.py
+++ 'module_1.4.ру' организация программ и методы строк.py
@@ -1,10 +1,10 @@
 
-my_string = (input('ведите название города: '))
+my_string = (input('ведите текст(минимум 3 слова): '))
 
 print(len(my_string))
 print(my_string.upper())
 print(my_string.lower())
-print(my_string.replace(' ', ' '))
+print(my_string.replace(' ', '#'))
 
 
 print(my_string[0])
